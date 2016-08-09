efc9fba35504aed1ebc976bf3952e346dbd243e9
zuogangju369 <zuogangju369@126.com>
2016-08-09 11:19:44 +0800

    测试json http://127.0.0.1/unionpayjpa/test

--- unionpayjpa/src/main/java/com/ajie/wechat/controller/UserController.java
+++ unionpayjpa/src/main/java/com/ajie/wechat/controller/UserController.java
@@ -3,15 +3,15 @@ package com.ajie.wechat.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.ajie.wechat.model.User;
 import com.ajie.wechat.service.UserService;
+import com.alibaba.fastjson.JSONObject;
 /**
  * 
  * @ClassName: UserController
@@ -20,7 +20,7 @@ import com.ajie.wechat.service.UserService;
  * @date 2016年8月3日 下午1:47:00
  *
  */
-@ControllerAdvice
+@RestController
 @RequestMapping(value = "")
 public class UserController {
 	@Autowired
@@ -28,8 +28,11 @@ public class UserController {
 
 	/**
 	 * 
-	 * @Title: saveUser @Description: 添加用户信息 post提交方式 @param user @return String
-	 * 返回类型 @throws
+	 * @Title: saveUser 
+	 * @Description: 添加用户信息 post提交方式 
+	 * @param user
+	 * @return  String    返回类型  
+	 * @throws
 	 */
 	@RequestMapping(value = "/saveUser", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE
 			+ ";charset=UTF-8")
@@ -38,8 +41,11 @@ public class UserController {
 	}
 	/**
 	 * 
-	 * @Title: add @Description: 添加用户信息 get方式提交 @param account @return String
-	 * 返回类型 @throws
+	 * @Title: add 
+	 * @Description: 添加用户信息 get方式提交
+	 * @param account
+	 * @return  String    返回类型  
+	 * @throws
 	 */
 	@RequestMapping(value = "/saveUser/{account}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE
 			+ ";charset=UTF-8")
@@ -48,24 +54,38 @@ public class UserController {
 	}
 	/**
 	 * 
-	 * @Title: add @Description: 根据id查询用户信息 @param id @return User 返回类型 @throws
+	 * @Title: get 
+	 * @Description: 根据id查询用户信息
+	 * @param id
+	 * @return  User    返回类型  
+	 * @throws
 	 */
-	@RequestMapping(value = "/user/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE
-			+ ";charset=UTF-8")
-	public User add(@PathVariable Long id) {
+	@RequestMapping(value = "/user/{id}")
+	public User get(@PathVariable Long id) {
 		return userService.getOneUser(id);
 	}
 	/**
 	 * 
-	 * @Title: test @Description: 测试 @return String 返回类型 @throws
+	 * @Title: test 
+	 * @Description: 测试 
+	 * @param mv
+	 * @return  ModelAndView    返回类型  
+	 * @throws
 	 */
 	@RequestMapping(value = "/test")
-	public ModelAndView test(ModelAndView mv) {
-		mv = new ModelAndView("WEB-INF/success");
-		mv.addObject("test", "ModelAndView");
-		System.out.println(123465);
-		return mv;
+	public JSONObject test() {
+		JSONObject json=new JSONObject();
+		json.put("test", "我是一个json");
+		return json;
 	}
+	/**
+	 * 
+	 * @Title: test2 
+	 * @Description: 返回页面
+	 * @param model
+	 * @return  String    返回类型  
+	 * @throws
+	 */
 	@RequestMapping(value = "/test2")
 	public String test2(Model model) {
 		model.addAttribute("test", "Model");
