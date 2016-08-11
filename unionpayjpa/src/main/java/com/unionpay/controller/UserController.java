e3e06cc2c299a84a010759ab6745c7ed2c6b6be5
zuogangju369 <zuogangju369@126.com>
2016-08-11 10:38:49 +0800

    20160811

--- unionpayjpa/src/main/java/com/unionpay/controller/UserController.java
+++ unionpayjpa/src/main/java/com/unionpay/controller/UserController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-
 import com.alibaba.fastjson.JSONObject;
 import com.unionpay.model.User;
 import com.unionpay.service.UserService;
